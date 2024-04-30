c072d1b673d961375bd87913bea2442158609d86
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-05-01 06:19:36 +0700

    Refactor dictionary

---
+++ src/Dictionary.java
@@ -0,0 +1,28 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.HashSet;
+
+class Dictionary {
+	private HashSet<String> words = new HashSet<>();
+
+	Dictionary(String filepath) {
+		try {
+			FileReader fileReader = new FileReader(filepath);
+			BufferedReader reader = new BufferedReader(fileReader);
+
+			String word = reader.readLine();
+			while (word != null) {
+				this.words.add(word);
+				word = reader.readLine();
+			}
+
+			fileReader.close();
+		} catch (Exception e) {
+			System.out.println("Can't read dictionary: " + filepath);
+		}
+	}
+
+	boolean checkWord(String word) {
+		return this.words.contains(word);
+	}
+}
