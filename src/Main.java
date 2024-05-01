7c57f3f5b3484cd0817910fd8502de6a4f62ad08
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-05-01 09:56:54 +0700

    Tracking visitor

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 import Algorithm.*;
 import Construct.*;
+import Construct.Node;
+
+class TrackingVisitor implements Visitor {
+	int visitedCounter = 0;
+
+	@Override
+	public void visit(Node node) {
+		System.out.println(node);
+		this.visitedCounter += 1;
+	}
+
+	public int getVisitedCounter() {
+		return this.visitedCounter;
+	}
+};
 
 class Main {
 	public static void main(String[] args) {
 		Dictionary dictionary = new Dictionary("dictionary.txt");
-		Visitor visitor = (node) -> {
-			System.out.println(node);
-		};
 
 		Finder finder = new Astar();
+		TrackingVisitor visitor = new TrackingVisitor();
 		System.out.println(finder.search("myth", "lore", dictionary, visitor));
+		System.out.println("Visited count: " + visitor.getVisitedCounter());
 	}
 }
